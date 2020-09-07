62545bae5a0073ff547a6b07632a7909e0d72bae
Sangeeta <sangeetakc012@gmail.com>
2020-09-07 21:56:22 +0530

    v2support issue please resolve

--- src/app/_view/home/home.module.ts
+++ src/app/_view/home/home.module.ts
@@ -4,10 +4,23 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LayoutModule } from '../layout/layout.module';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
+import { ResponsiveComponent } from './responsive/responsive.component';
+import { ThreePartsComponent } from './three-parts/three-parts.component';
+import { LatestWorkComponent } from './latest-work/latest-work.component';
+import { SliderComponent } from './slider/slider.component';
+import { LogoComponent } from './logo/logo.component';
+import { StrategyComponent } from './strategy/strategy.component';
+import { OurTeamComponent } from './our-team/our-team.component';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { MagazineComponent } from './magazine/magazine.component';
+import { InspiredComponent } from './inspired/inspired.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { SeparateLineComponent } from './separate-line/separate-line.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeBannerComponent, ResponsiveComponent, ThreePartsComponent, LatestWorkComponent, SliderComponent, LogoComponent, StrategyComponent, OurTeamComponent, BenefitsComponent, MagazineComponent, InspiredComponent, SubscribeComponent, SeparateLineComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
---
+++ src/app/_view/home/inspired/inspired.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inspired',
+  templateUrl: './inspired.component.html',
+  styleUrls: ['./inspired.component.scss']
+})
+export class InspiredComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
