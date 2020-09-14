aececb4ebdf3c08cda73b3726624cbeec4f44a76
Sangeeta <sangeetakc012@gmail.com>
2020-09-14 21:53:45 +0530

    portfolio page

--- src/app/_view/portfolio/portfolio-tabs/portfolio-tabs.component.ts
+++ src/app/_view/portfolio/portfolio-tabs/portfolio-tabs.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioTabsComponent implements OnInit {
 
+  selectedLearning = "all";
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  activeLearning(active) {
+    this.selectedLearning = active;
   }
 
 }
